b625d649897b5f9e4902bcc7be97d03be63a3cae
virendra-978 <virendrat2121@gmail.com>
2026-02-25 15:49:50 +0530

    added historical_join

---
+++ sql/07_historical_join.sql
@@ -0,0 +1,58 @@
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE SNOWFLAKE_LEARNING_DB;
+
+--Adding transaction date to silver Fact 
+CREATE OR REPLACE TABLE SILVER.STORE_SALES_ENRICHED AS
+SELECT
+    ss.ss_customer_sk,
+    ss.ss_store_sk,
+    ss.ss_sales_price,
+    d.d_date AS transaction_date
+FROM SILVER.STORE_SALES_CLEAN ss
+JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.DATE_DIM d
+    ON ss.ss_sold_date_sk = d.d_date_sk;
+
+--Proper historical join
+SELECT
+    f.transaction_date,
+    d.state,
+    SUM(f.ss_sales_price) AS total_sales
+FROM SILVER.STORE_SALES_ENRICHED f
+JOIN SILVER.CUSTOMER_DIM d
+    ON f.ss_customer_sk = d.customer_id
+    AND f.transaction_date >= d.start_date
+    AND (f.transaction_date < d.end_date OR d.end_date IS NULL)
+GROUP BY
+    f.transaction_date,
+    d.state
+ORDER BY
+    f.transaction_date;
+
+
+-- simulate the incremental fact load
+
+CREATE OR REPLACE TABLE SILVER.FACT_STAGE (
+    customer_id NUMBER,
+    store_id NUMBER,
+    sales_price NUMBER,
+    transaction_date DATE
+);
+
+-- insert the increemntal data
+INSERT INTO SILVER.FACT_STAGE VALUES
+(101, 10, 500, '2024-01-10'),
+(101, 10, 700, '2021-01-10');
+
+-- Load the facts with surrogate keys
+
+CREATE OR REPLACE TABLE SILVER.FACT_SALES AS
+SELECT
+    d.customer_sk,
+    f.store_id,
+    f.sales_price,
+    f.transaction_date
+FROM SILVER.FACT_STAGE f
+JOIN SILVER.CUSTOMER_DIM d
+    ON f.customer_id = d.customer_id
+    AND f.transaction_date >= d.start_date
+    AND (f.transaction_date < d.end_date OR d.end_date IS NULL);
