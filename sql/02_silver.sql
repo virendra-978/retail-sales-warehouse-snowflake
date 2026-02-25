87e433d0b0344c7932fec1f0b221118ac91eb1a1
virendra-978 <virendrat2121@gmail.com>
2026-02-25 12:33:51 +0530

    added silver architecture

---
+++ sql/02_silver.sql
@@ -0,0 +1,38 @@
+USE WAREHOUSE COMPUTE_WH;
+
+USE DATABASE SNOWFLAKE_LEARNING_DB;
+
+CREATE SCHEMA IF NOT EXISTS SILVER;
+CREATE OR REPLACE TABLE SILVER.STORE_SALES_CLEAN AS 
+SELECT 
+    ss.ss_sold_date_sk,
+    ss.ss_store_sk,
+    ss.ss_customer_sk,
+    ss.ss_quantity,
+    ss.ss_sales_price
+FROM BRONZE.STORE_SALES_RAW ss
+JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.DATE_DIM d
+    ON ss.ss_sold_date_sk = d.d_date_sk
+WHERE d.d_year BETWEEN 1998 AND 1999
+AND ss.ss_sales_price IS NOT NULL
+AND ss.ss_quantity > 0;
+
+DESC TABLE SILVER.STORE_SALES_CLEAN;
+
+SELECT MIN(ss_sales_price), MAX(ss_sales_price)
+FROM SILVER.STORE_SALES_CLEAN;
+
+--Validation through row count
+SELECT COUNT(*) FROM BRONZE.STORE_SALES_RAW;
+SELECT COUNT(*) FROM SILVER.STORE_SALES_CLEAN;
+
+--Null Check
+SELECT COUNT(*)
+FROM SILVER.STORE_SALES_CLEAN
+WHERE ss_sales_price IS NULL;
+
+--Negative Check
+SELECT COUNT(*)
+FROM SILVER.STORE_SALES_CLEAN
+WHERE ss_quantity <= 0;
+
