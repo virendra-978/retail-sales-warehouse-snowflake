45e9057f2b0b704a844e5ee1637050a6f89bf9ba
virendra-978 <virendrat2121@gmail.com>
2026-02-26 10:37:27 +0530

    Updated historical_join

--- sql/07_historical_join.sql
+++ sql/07_historical_join.sql
@@ -45,14 +45,39 @@ INSERT INTO SILVER.FACT_STAGE VALUES
 
 -- Load the facts with surrogate keys
 
+DROP TABLE IF EXISTS SILVER.CUSTOMER_DIM;
+
+CREATE OR REPLACE TABLE SILVER.CUSTOMER_DIM (
+    customer_sk NUMBER AUTOINCREMENT,
+    customer_id NUMBER,
+    state STRING,
+    start_date DATE,
+    end_date DATE,
+    is_current STRING
+);
+
+INSERT INTO SILVER.CUSTOMER_DIM
+(customer_id, state, start_date, end_date, is_current)
+SELECT
+    c.c_customer_sk,
+    ca.ca_state,
+    '1900-01-01',
+    NULL,
+    'Y'
+FROM SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER c
+JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER_ADDRESS ca
+    ON c.c_current_addr_sk = ca.ca_address_sk;
+
 CREATE OR REPLACE TABLE SILVER.FACT_SALES AS
 SELECT
     d.customer_sk,
-    f.store_id,
-    f.sales_price,
+    f.ss_store_sk AS store_id,
+    f.ss_sales_price AS sales_price,
     f.transaction_date
-FROM SILVER.FACT_STAGE f
+FROM SILVER.STORE_SALES_ENRICHED f
 JOIN SILVER.CUSTOMER_DIM d
-    ON f.customer_id = d.customer_id
+    ON f.ss_customer_sk = d.customer_id
     AND f.transaction_date >= d.start_date
     AND (f.transaction_date < d.end_date OR d.end_date IS NULL);
+
+SELECT * FROM SILVER.FACT_SALES;
